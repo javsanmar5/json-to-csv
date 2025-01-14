786dcd6ac7a48598674efa3b6dca0795f4c2fb02
Javier Santos <javsanmar5@alum.us.es>
2025-01-14 15:38:44 +0100

    Add input and output file management

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
-fn main() {
-    println!("Hello, world!");
+use std::fs::File;
+use std::io::{Read, Write};
+
+static INPUT_PATH: &str = "data/data.json";
+static OUTPUT_PATH: &str = "data/output.csv";
+
+
+fn main() -> std::io::Result<()> {
+    let mut input_file = File::open(INPUT_PATH)?;
+    let mut output_file = File::create(OUTPUT_PATH)?;
+    let mut data = vec![];
+    
+    input_file.read_to_end(&mut data)?;
+
+    if let Ok(content) = String::from_utf8(data.clone()) {
+        println!("{}", content);
+    } else {
+        eprintln!("Error: Invalid UTF-8 sequence in input file");
+    }
+    
+    output_file.write_all(&data)?;
+
+    Ok(())
 }
